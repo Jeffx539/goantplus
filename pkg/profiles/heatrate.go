97268713cd5aab934442e2e72c4bc2f56b1a4199
Jeff M <jeff@x539.me>
2020-01-21 19:26:28 +1100

    Added HR Profile

---
+++ pkg/profiles/heatrate.go
@@ -0,0 +1,29 @@
+package profiles
+
+import "fmt"
+
+type HeartRate struct {
+	pageNumber     byte
+	PageSpecific0  byte
+	PageSpecific1  byte
+	PageSpecific2  byte
+	HRLSB          byte
+	HRMSB          byte
+	heartBeatCount byte
+	heartRate      byte
+}
+
+func (h *HeartRate) UnMarshal(payload []byte) {
+	h.pageNumber = payload[2]
+	h.PageSpecific0 = payload[3]
+	h.PageSpecific1 = payload[4]
+	h.PageSpecific2 = payload[5]
+	h.HRLSB = payload[6]
+	h.HRMSB = payload[7]
+	h.heartBeatCount = payload[8]
+	h.heartRate = payload[9]
+}
+
+func (h HeartRate) String() string {
+	return fmt.Sprintf("[HRM] Heartrate %d %f", h.heartRate)
+}
